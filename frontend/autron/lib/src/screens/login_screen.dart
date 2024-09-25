a6c5fcc706ea35a5779472c81fb14e8483396c3d
AndKlet <andreaskletvang@outlook.com>
2024-09-25 05:25:12 +0200

    feat: update login screen to use auth service

--- frontend/autron/lib/src/screens/login_screen.dart
+++ frontend/autron/lib/src/screens/login_screen.dart
@@ -1,28 +1,36 @@
+import 'package:autron/src/services/auth_service.dart';
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
-  const LoginPage({Key? key}) : super(key: key);
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
 
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final AuthService _authService = AuthService();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[300],
       body: SafeArea(
         child: Center(
-          child: Column(children: [
-            const SizedBox(height: 200),
-            //logo
-            Image.asset('assets/images/autronica-logo.png',
-              scale: 2,
-            ), 
-            const SizedBox(height: 50),
-            ElevatedButton(
-                onPressed: () {
-                  // Login Logic
-                },
+          child: Column(
+            children: [
+              const SizedBox(height: 200),
+              // Logo
+              Image.asset(
+                'assets/images/autronica-logo.png',
+                scale: 2,
+              ),
+              const SizedBox(height: 50),
+              ElevatedButton(
+                onPressed: _authService.login,
                 style: ElevatedButton.styleFrom(
                   backgroundColor: const Color(0xFF005E1D),
-                  padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 50, vertical: 15),
                   shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(8),
                   ),
@@ -35,7 +43,7 @@ class LoginPage extends StatelessWidget {
                   ),
                 ),
               ),
-          ],
+            ],
           ),
         ),
       ),
