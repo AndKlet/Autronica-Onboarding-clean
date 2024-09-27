3d1d4bcf4e7153d7226d4ac253a18d6b4b1093b7
Eduard Prokhorikhin <bordduk69@dhcp-10-22-127-32.wlan.ntnu.no>
2024-09-27 10:12:57 +0200

    refactor: made positioning of elements more scalable and cleaner

--- frontend/autron/lib/src/screens/home.dart
+++ frontend/autron/lib/src/screens/home.dart
@@ -7,71 +7,64 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(color: Colors.white),
-      child: const Stack(
+    return Center(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Positioned(
-            left: 30,
-            top: 140,
-            child: SizedBox(
-              width: 200,
-              height: 50,
-              child: Text(
-                'Hi JOHN',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 48,
-                  fontFamily: 'Inter',
-                  fontWeight: FontWeight.w700,
-                  height: 0.02,
-                  letterSpacing: -0.96,
-                ),
+          Container(
+            width: 200,
+            height: 60,
+            margin: const EdgeInsets.only(top: 30),
+            child: Text(
+              'Hi JOHN',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 48,
+                fontFamily: 'Inter',
+                fontWeight: FontWeight.w700,
               ),
             ),
           ),
-          Positioned(
-            left: 32,
-            top: 190,
-            child: SizedBox(
-              width: 127,
-              height: 20,
-              child: Text(
-                'Department',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 20,
-                  fontFamily: 'Inter',
-                  fontWeight: FontWeight.w400,
-                  height: 0.06,
-                ),
+          Container(
+            width: 127,
+            height: 25,
+            margin: const EdgeInsets.only(top: 20),
+            child: Text(
+              'Department',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 20,
+                fontFamily: 'Inter',
+                fontWeight: FontWeight.w400,
               ),
             ),
           ),
-          Positioned(
-            left: 46,
-            top: 250,
-            child: SizedBox(
-              child: Announcement(
-                title: 'Announcement',
-                description:
-                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim  veniam, quis nostrud exercitation ullamco.',
-              ),
+          Container(
+            margin: const EdgeInsets.only(top: 20),
+            child: const Announcement(
+              title: 'Announcement',
+              description:
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim  veniam, quis nostrud exercitation ullamco.',
             ),
           ),
-          
-          
-          Positioned(
-            left: 46,
-            top: 502,
-            child: StatusAlert(title: "Software granted", value: "Currently", count: 17, color: Color(0xFF5AB443)),
-          ),
-          
-          Positioned(
-            left: 46,
-            top: 621,
-            child: StatusAlert(title: "Pending Requests", value: "Currently", count: 5, color: Color(0xFFE2CE4B)),
+          Container(
+            margin: const EdgeInsets.only(top: 30),
+            child: const StatusAlert(
+              title: "Software granted", 
+              value: "Currently", 
+              count: 17, 
+              color: Color(0xFF5AB443)
             ),
+          ),
+          Container(
+            margin: const EdgeInsets.only(top: 20),
+            child: const StatusAlert(
+              title: "Pending Requests", 
+              value: "Currently", 
+              count: 5, 
+              color: Color(0xFFE2CE4B)
+            ),
+          ),
         ],
       ),
     );
