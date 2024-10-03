01edd9f0f9449aa8e88e83afc50a4adea8e3a303
Magnus Vik <magnusfv@stud.ntnu.no>
2024-10-03 17:11:57 +0200

    tweaked the software boxes and added a hardcoded page with information about softwares and the *request access* button

--- frontend/autron/lib/src/widgets/software_box.dart
+++ frontend/autron/lib/src/widgets/software_box.dart
@@ -18,17 +18,21 @@ class SoftwareBox extends StatelessWidget {
       child: Container(
         margin: const EdgeInsets.all(8.0),
         padding: const EdgeInsets.all(16.0),
+        width: 160, // Fixed width for each box
+        height: 120, // Fixed height for each box
         decoration: BoxDecoration(
-          color: Colors.grey[300],
+          color: const Color.fromARGB(70, 22, 58, 167),
           borderRadius: BorderRadius.circular(12),
         ),
         child: Center(
           child: Text(
             softwareName,
-            style: TextStyle(
+            style: const TextStyle(
               fontSize: 16,
               fontWeight: FontWeight.bold,
+              color: Colors.black,
             ),
+            textAlign: TextAlign.center,
           ),
         ),
       ),
