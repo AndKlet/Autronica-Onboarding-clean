fca91df618b4178fd3bdf775a5a65e8ae26a5792
Eduard Prokhorikhin <eduardprokhorikhin@gmail.com>
2024-10-04 13:01:58 +0200

    feat: created model for department as it was missing priviously

--- backend/autron/autron/models.py
+++ backend/autron/autron/models.py
@@ -44,3 +44,9 @@ class Software(models.Model):
 
     def __str__(self):
         return self.name
+    
+class Department(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
