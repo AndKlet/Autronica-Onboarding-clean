80be96969415499e9ffe19b16db09ca23c4e0665
Eduard Prokhorikhin <eduardprokhorikhin@gmail.com>
2024-10-09 12:12:21 +0200

    style: linting on all files

--- backend/autron/autron/models.py
+++ backend/autron/autron/models.py
@@ -1,9 +1,11 @@
-from django.db import models
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
+from django.db import models
 
-# Generic request resolver model for fields that are common to all request resolvers. 
-# You are not to create an instance of this model directly. Instead, you should create an instance of a subclass of this model.
+
+# Generic request resolver model for fields that are common to all request resolvers.
+# You are not to create an instance of this model directly.
+# Instead, you should create an instance of a subclass of this model.
 class RequestResolver(models.Model):
     name = models.CharField(max_length=100)
 
@@ -12,9 +14,10 @@ class RequestResolver(models.Model):
 
     def __str__(self):
         return self.name
-    
 
-# Subclasses of RequestResolver model. This resolver will be used for the software that need to send an email to request access.
+
+# Subclasses of RequestResolver model.
+# This resolver will be used for the software that need to send an email to request access.
 class EmailRequestResolver(RequestResolver):
     email_address = models.EmailField()
     email_subject = models.CharField(max_length=100)
@@ -24,27 +27,31 @@ class EmailRequestResolver(RequestResolver):
         # Logic to send an email
         pass
 
-# Subclasses of RequestResolver model. This resolver will be used for the software that need to call an external API (servicenow) to request access.
+
+# Subclasses of RequestResolver model.
+# This resolver will be used for the software that need to call an external API (servicenow)
+# to request access.
 class APIRequestResolver(RequestResolver):
     api_endpoint = models.URLField()
-    
 
     def handle_request(self, request):
         # Logic to call an external API
         pass
 
+
 # Software model that uses the generic foreign key to link to the resolver model.
 class Software(models.Model):
     name = models.CharField(max_length=100)
-    department = models.ForeignKey('Department', on_delete=models.CASCADE)
+    department = models.ForeignKey("Department", on_delete=models.CASCADE)
     description = models.TextField()
     resolver_content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     resolver_object_id = models.PositiveIntegerField()
-    resolver = GenericForeignKey('resolver_content_type', 'resolver_object_id')
+    resolver = GenericForeignKey("resolver_content_type", "resolver_object_id")
 
     def __str__(self):
         return self.name
-    
+
+
 class Department(models.Model):
     name = models.CharField(max_length=100)
 
--- backend/autron/autron/serializers.py
+++ backend/autron/autron/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+
 from .models import *
 
 # https://www.django-rest-framework.org/api-guide/serializers/
