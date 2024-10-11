31888222a383763a3641179bb8d0284e6d06a733
Magnus Vik <magnusfv@stud.ntnu.no>
2024-10-11 12:24:20 +0200

    Tweaked the design of the navbar

--- frontend/autron/lib/src/app.dart
+++ frontend/autron/lib/src/app.dart
@@ -1,15 +1,12 @@
+import 'package:flutter/material.dart';
 import 'package:autron/src/screens/request_screen.dart';
 import 'package:autron/src/screens/home.dart';
 import 'package:autron/src/screens/login_screen.dart';
 import 'package:autron/src/screens/user_screen.dart';
 import 'package:autron/src/screens/software_screen.dart';
-import 'package:flutter/material.dart';
 
-/// The Widget that configures your application.
 class MyApp extends StatefulWidget {
-  const MyApp({
-    super.key,
-  });
+  const MyApp({super.key});
 
   @override
   State<MyApp> createState() => _MyAppState();
@@ -33,57 +30,88 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        routes: {
-          '/home': (context) => const MyApp(),
-          '/login': (context) => const LoginPage(),
-          '/software': (context) => const SoftwarePage(),
-        },
-        debugShowCheckedModeBanner: false,
-
-        // Navigation bar theme
-        theme: ThemeData(
-          navigationBarTheme: NavigationBarThemeData(
-            backgroundColor: const Color(0xFFF9F9F9),
-            indicatorColor: const Color.fromARGB(90, 0, 0, 0),
-            indicatorShape: const RoundedRectangleBorder(
-              borderRadius: BorderRadius
-                  .zero, // Removes rounded corners to make it rectangular
+      routes: {
+        '/home': (context) => const MyApp(),
+        '/login': (context) => const LoginPage(),
+        '/software': (context) => const SoftwarePage(),
+      },
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: _screens[_selectedIndex],
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _selectedIndex,
+          onTap: _onItemTapped,
+          selectedItemColor: Colors.white,
+          unselectedItemColor: const Color(0xFF005E1D),
+          backgroundColor: const Color(0xFFF9F9F9),
+          type: BottomNavigationBarType.fixed,
+          items: [
+            _buildBottomNavItem(
+              icon: Icons.home,
+              label: 'Home',
+              index: 0,
+              isSelected: _selectedIndex == 0,
             ),
-            labelTextStyle: WidgetStateProperty.all(
-              const TextStyle(color: Color(0xFF005E1D)),
+            _buildBottomNavItem(
+              icon: Icons.my_library_add,
+              label: 'Software',
+              index: 1,
+              isSelected: _selectedIndex == 1,
             ),
-            iconTheme: WidgetStateProperty.all(
-              const IconThemeData(color: Color(0xFF005E1D)),
+            _buildBottomNavItem(
+              icon: Icons.assignment,
+              label: 'Requests',
+              index: 2,
+              isSelected: _selectedIndex == 2,
             ),
-          ),
+            _buildBottomNavItem(
+              icon: Icons.account_circle,
+              label: 'Profile',
+              index: 3,
+              isSelected: _selectedIndex == 3,
+            ),
+          ],
         ),
-        home: Scaffold(
-          body: _screens[_selectedIndex],
-          bottomNavigationBar: NavigationBar(
-            selectedIndex: _selectedIndex,
-            onDestinationSelected: _onItemTapped,
-            labelBehavior: NavigationDestinationLabelBehavior.alwaysShow,
-            backgroundColor: const Color(0xFFF9F9F9),
-            indicatorColor: const Color.fromARGB(90, 0, 0, 0),
-            destinations: const [
-              NavigationDestination(
-                icon: Icon(Icons.home, color: Color(0xFF005E1D)),
-                label: 'Home',
+      ),
+    );
+  }
+
+  // Custom method to build BottomNavigationBarItem
+  BottomNavigationBarItem _buildBottomNavItem({
+    required IconData icon,
+    required String label,
+    required int index,
+    required bool isSelected,
+  }) {
+    return BottomNavigationBarItem(
+      icon: Container(
+        padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 24.0),
+        decoration: BoxDecoration(
+          color: isSelected ? Colors.grey[300] : Colors.transparent,
+          borderRadius: BorderRadius.circular(4.0),
+        ),
+        child: Column(
+          children: [
+            Icon(
+              icon,
+              size: 28,
+              color: isSelected
+                  ? const Color.fromARGB(255, 0, 0, 0)
+                  : const Color(0xFF005E1D),
+            ),
+            Text(
+              label,
+              style: TextStyle(
+                fontSize: 12,
+                color: isSelected
+                    ? const Color.fromARGB(255, 0, 0, 0)
+                    : const Color(0xFF005E1D),
               ),
-              NavigationDestination(
-                icon: Icon(Icons.add_circle_outline, color: Color(0xFF005E1D)),
-                label: 'Software',
-              ),
-              NavigationDestination(
-                icon: Icon(Icons.assignment, color: Color(0xFF005E1D)),
-                label: 'Requests',
-              ),
-              NavigationDestination(
-                icon: Icon(Icons.account_circle, color: Color(0xFF005E1D)),
-                label: 'User',
-              ),
-            ],
-          ),
-        ));
+            ),
+          ],
+        ),
+      ),
+      label: '',
+    );
   }
 }
