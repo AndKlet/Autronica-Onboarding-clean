6490cea17bfbd06bbedaf96f3cb256b7b210ebaa
julleeee <93137568+julleeee@users.noreply.github.com>
2024-10-11 13:11:05 +0200

    feat: updated models for json parsing

--- frontend/autron/lib/src/view_models/department_model.dart
+++ frontend/autron/lib/src/view_models/department_model.dart
@@ -1,5 +1,20 @@
 class Department {
+  int id;
   String name;
 
-  Department({required this.name});
+  Department({required this.name, required this.id});
+
+  factory Department.fromJson(Map<String, dynamic> json) {
+    return switch (json) {
+      {
+        'id': int id,
+        'name': String name,
+      } =>
+        Department(
+          id: id,
+          name: name,
+        ),
+      _ => throw const FormatException('Failed to load department.'),
+    };
+  }
 }
--- frontend/autron/lib/src/view_models/software_model.dart
+++ frontend/autron/lib/src/view_models/software_model.dart
@@ -1,8 +1,25 @@
 import 'package:autron/src/view_models/department_model.dart';
 
 class Software {
+  int id;
   String name;
   Department department;
 
-  Software({required this.name, required this.department});
+  Software({required this.name, required this.department, required this.id});
+
+  factory Software.fromJson(Map<String, dynamic> json) {
+    return switch (json) {
+      {
+        'id': int id,
+        'name': String name,
+        'department': Department department,
+      } =>
+        Software(
+          id: id,
+          name: name,
+          department: department,
+        ),
+      _ => throw const FormatException('Failed to load software.'),
+    };
+  }
 }
