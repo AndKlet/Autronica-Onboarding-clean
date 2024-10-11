3e7f83ab95c11fd5a755082b6443b59d7c66eefd
julleeee <93137568+julleeee@users.noreply.github.com>
2024-10-11 12:36:18 +0200

    feat: made software model in flutter

---
+++ frontend/autron/lib/src/view_models/software_model.dart
@@ -0,0 +1,8 @@
+import 'package:autron/src/view_models/department_model.dart';
+
+class Software {
+  String name;
+  Department department;
+
+  Software({required this.name, required this.department});
+}
