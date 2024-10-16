f52176953db39253396a889dfd1430336296dda5
julleeee <93137568+julleeee@users.noreply.github.com>
2024-10-16 12:51:27 +0200

    feat: updated model to match fields of API

--- frontend/autron/lib/src/view_models/software_model.dart
+++ frontend/autron/lib/src/view_models/software_model.dart
@@ -3,23 +3,22 @@ import 'package:autron/src/view_models/department_model.dart';
 class Software {
   int id;
   String name;
+  String? status; // Optional status
   Department department;
 
-  Software({required this.name, required this.department, required this.id});
+  Software({
+    required this.id,
+    required this.name,
+    this.status, // Optional status
+    required this.department,
+  });
 
   factory Software.fromJson(Map<String, dynamic> json) {
-    return switch (json) {
-      {
-        'id': int id,
-        'name': String name,
-        'department': Department department,
-      } =>
-        Software(
-          id: id,
-          name: name,
-          department: department,
-        ),
-      _ => throw const FormatException('Failed to load software.'),
-    };
+    return Software(
+      id: json['id'] as int,
+      name: json['name'] as String,
+      status: json.containsKey('status') ? json['status'] as String? : null,
+      department: Department.fromJson(json['department']),
+    );
   }
 }
