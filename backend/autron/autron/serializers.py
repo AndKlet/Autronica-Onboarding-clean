966989cf9ffc178377d0c3b67d14726f47ea779c
Eduard Prokhorikhin <eduardprokhorikhin@gmail.com>
2024-10-18 13:16:36 +0200

    feat: added meta fields to serializers

--- backend/autron/autron/serializers.py
+++ backend/autron/autron/serializers.py
@@ -4,10 +4,20 @@ from .models import Software, Department
 class DepartmentSerializer(serializers.ModelSerializer):
     id = serializers.IntegerField()
     name = serializers.CharField(max_length=100)
+    
+    class Meta:
+        model = Department
+        fields = "__all__"
 
 class SoftwareSerializer(serializers.ModelSerializer):
     id = serializers.IntegerField()
     name = serializers.CharField(max_length=100)
     department = DepartmentSerializer()
     image = serializers.ImageField(required=False)
+    
+    class Meta:
+        model = Software
+        fields = "__all__"
+        
+    
 
