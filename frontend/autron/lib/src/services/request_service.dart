1f20d181e3bf8635916ca66de73c28d17753fa7c
julleeee <93137568+julleeee@users.noreply.github.com>
2024-10-21 15:38:29 +0200

    feat: created request service.dart

--- frontend/autron/lib/src/services/request_service.dart
+++ frontend/autron/lib/src/services/request_service.dart
@@ -43,4 +43,21 @@ class RequestService {
     final allRequest = await getAllRequests();
     return allRequest.where((request) => request.status == status).toList();
   }
+}import 'dart:convert';
+import 'package:autron/src/view_models/request_model.dart';
+import 'package:http/http.dart' as http;
+
+class RequestService {
+  Future<void> requestSoftware(Request request) async {
+    final response = await http.post(
+      Uri.parse('http://10.0.2.2:8000/request_software/'),
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode(request.toJson()),
+    );
+    if (response.statusCode == 200) {
+      return;
+    } else {
+      throw Exception('Failed to request software');
+    }
+  }
 }
