909b597ef80af0576014e54b451db251512a98b9
julleeee <93137568+julleeee@users.noreply.github.com>
2024-10-28 10:42:37 +0100

    feat: remove request_software as it is already implemented in dev

--- frontend/autron/lib/src/services/request_service.dart
+++ frontend/autron/lib/src/services/request_service.dart
@@ -3,20 +3,6 @@ import 'package:autron/src/view_models/request_model.dart';
 import 'package:http/http.dart' as http;
 
 class RequestService {
-  Future<void> requestSoftware(Request request) async {
-    final response = await http.post(
-      Uri.parse(
-          'https://164.92.218.9/request_software/${request.software.id}/'),
-      headers: {'Content-Type': 'application/json'},
-      body: jsonEncode(request.toJson()),
-    );
-    if (response.statusCode == 200) {
-      return;
-    } else {
-      throw Exception('Failed to request software');
-    }
-  }
-
   /// Gets a list of requests.
   Future<List<Request>> getAllRequests() async {
     final response =
