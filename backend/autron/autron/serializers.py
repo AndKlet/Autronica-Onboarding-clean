f1753a44b604270d5ee21bf328e919411fe2ba72
Anna Østmo <annaost@stud.ntnu.no>
2024-10-29 11:54:54 +0100

    added description in database

--- backend/autron/autron/serializers.py
+++ backend/autron/autron/serializers.py
@@ -1,23 +1,24 @@
 from rest_framework import serializers
-from .models import Software, Department
+
+from .models import Department, Software
+
 
 class DepartmentSerializer(serializers.ModelSerializer):
     id = serializers.IntegerField()
     name = serializers.CharField(max_length=100)
-    
+
     class Meta:
         model = Department
         fields = "__all__"
+
 
 class SoftwareSerializer(serializers.ModelSerializer):
     id = serializers.IntegerField()
     name = serializers.CharField(max_length=100)
     department = DepartmentSerializer()
     image = serializers.ImageField(required=False)
-    
+    description = serializers.CharField(max_length=500)
+
     class Meta:
         model = Software
         fields = "__all__"
-        
-    
-
