33f9a5358f183f2bb6f8115c6303285967249664
AndKlet <andreaskletvang@outlook.com>
2024-11-01 13:05:10 +0100

    feat: add method to fetch user data from server

--- frontend/autron/lib/src/services/auth_service.dart
+++ frontend/autron/lib/src/services/auth_service.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/foundation.dart';
 import 'dart:developer' as developer;
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 /// A service class that handles authentication.
 class AuthService {
@@ -19,5 +21,26 @@ class AuthService {
   /// - On mobile: Logs out using the specified scheme.
   Future<void> logout() async {
     print('Logging out...');
+  }
+}
+
+  /// Fetches user data using the provided access token.
+  /// 
+  /// - [accessToken]: The access token to use for fetching user data.
+  Future<void> fetchUserData(String accessToken) async {
+  final url = Uri.parse('https://164.92.218.9/get_user_data/');
+
+  final response = await http.get(
+    url,
+    headers: {
+      'Authorization': 'Bearer $accessToken',  // Send token in Authorization header
+    },
+  );
+
+  if (response.statusCode == 200) {
+    final userData = jsonDecode(response.body);
+    print('User Data Retrieved: $userData');
+  } else {
+    print('Failed to fetch user data: ${response.statusCode}');
   }
 }
