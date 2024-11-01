0b0a52e9f535f6f4a52f3d4aaabbbb94bc5913ab
AndKlet <andreaskletvang@outlook.com>
2024-11-01 13:13:00 +0100

    feat: add url for fetching user data

--- backend/autron/autron/urls.py
+++ backend/autron/autron/urls.py
@@ -66,6 +66,7 @@ urlpatterns = [
     path("request_access/", request_access_view, name="request_access"),
     path("request_software/<int:software_id>/", views.request_software, name="request_software"),
     path("request_list/", views.request_list, name="request_list"),
+    path("get_user_data/", views.get_user_data, name="get_user_data"),
     # Our app's urls here
     # path('/', include('autron.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
