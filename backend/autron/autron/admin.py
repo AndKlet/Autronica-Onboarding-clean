adfef1dc61015ccee3ee57884d1cb3f3a526e660
Eduard Prokhorikhin <eduardprokhorikhin@gmail.com>
2024-11-15 12:07:02 +0100

    feat: added requests model to django admin

--- backend/autron/autron/admin.py
+++ backend/autron/autron/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import RequestResolver, EmailRequestResolver, APIRequestResolver, Software, Department
+from .models import RequestResolver, EmailRequestResolver, APIRequestResolver, Software, Department, Request
 
 admin.site.register(RequestResolver)
 admin.site.register(EmailRequestResolver)
 admin.site.register(APIRequestResolver)
 admin.site.register(Software)
 admin.site.register(Department)
+admin.site.register(Request)
+
